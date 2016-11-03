0d2c38680fd32ec549a3f7bab7e5e646ff57916b
Yarrney <alex.arney@gmail.com>
2016-11-03 22:18:04 +1100

    developed basic css structure for widget

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-(function() {
+// Get contacts from JSON file
+var request = new XMLHttpRequest();
+   request.open("GET", "contact-list-users.json", false);
+   request.send(null);
+   var contacts = JSON.parse(request.responseText);
+   alert (my_JSON_object.result[0]);
 
-    $('#live-chat header').on('click', function() {
-
-        $('.chat').slideToggle(300, 'swing');
-
-    });
-
-    $('.contacts-close').on('click', function(e) {
-
-        e.preventDefault();
-        $('#live-chat').fadeOut(300);
-
-    });
-
-}) ();
+//
+data.images.forEach( function(obj) {
+  var img = new Image();
+  img.src = obj.bannerImg1;
+  img.setAttribute("class", "banner-img");
+  img.setAttribute("alt", "effy");
+  document.getElementById("img-container").appendChild(img);
+});
+//contacts.user.username
+//contacts.user.status
+//contacts.user.profileimage
