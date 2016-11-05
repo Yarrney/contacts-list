d5c9a576ce0847db8d9e98f0b33636ae70b2b3c6
Yarrney <alex.arney@gmail.com>
2016-11-06 09:54:03 +1100

    added less files

--- script.js
+++ script.js
@@ -48,22 +48,6 @@ function init() {
 
 init();
 
-// function loadJSON(callback) {
-//
-//     var xobj = new XMLHttpRequest();
-//         xobj.overrideMimeType("application/json");
-//     xobj.open('GET', 'contact-list-users.json', true);
-//     xobj.onreadystatechange = function () {
-//           if (xobj.readyState == 4 && xobj.status == "200") {
-//             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-//             callback(xobj.responseText);
-//           }
-//     };
-//     xobj.send(null);
-//  }
-//
-
-
 // CONTACT LIST TOGGLE
 function slideToggle() {
     var contactList = document.getElementById('hide-container');
