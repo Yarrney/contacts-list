a6b5cf02f9a4b90b036d068c625fb195b2930a1e
Yarrney <alex.arney@gmail.com>
2016-11-05 23:22:32 +1100

    updated HTML structure and CSS to keep search bar fixed - also modified JSON file for simpler parsing

--- script.js
+++ script.js
@@ -1,49 +1,73 @@
 // Get contacts from JSON file
-// var contactPopulate = function (contactList) {
-//   var contact = '';
-//   var profile = '';
-//   for (var i = 0; i < contactList.length; i++) {
-//     contact = contactList[i];
-//     //contact.status
-//     //contact.profileimage
-//     if (contact.profileimage === undefined) {
-//       profile = '<div class="contact-placeholder">'
-//         +contact.name
-//         +'</div>';
-//       //get first letter of name
-//     }
-//     else {
-//       profile = '<img src="'+contact.profileimage+'"/>';
-//       //contact.profileimage;
-//     }
-//   }
-//   '<li class="contacts">'
-//     +'<div class="contact-image">'
-//       +profile
-//     +'</div>'
-//     +'<p class="contact-name">'+contact.username+'</p>'
-//     +'<div class="contact-status-'+contact.status+'"></div>"'
-//   +'</li>'
-//   //contactList.user.username
-//   //contactList.user.status
-//   //contactList.user.profileimage
-// }
+var contactPopulate = function (contactList) {
+  var contact = '';
+  var profilePic = '';
+  for (var i = 0; i < contactList.length; i++) {
+    contact = contactList[i];
+    if (contact.profileimage === undefined) {
+      profilePic = '<span class="contact-placeholder">'
+        +contact.username.substring(0,1) //get first letter of name
+        +'</span>';
+    }
+    else {
+      profilePic = '<img src="'+contact.profileimage+'"/>';
+    }
+
+    document.getElementById('contacts-list').innerHTML +=
+    '<li class="contact">'
+      +'<div class="contact-image">'
+        +profilePic
+      +'</div>'
+      +'<p class="contact-name">'+contact.username+'</p>'
+      +'<div class="contact-status-'+contact.status.toLowerCase()+'"></div>'
+    +'</li>'
+
+  }
+}
 
 // Async request
-var xhr = new XMLHttpRequest();
-   xhr.open("GET", "contact-list-users.json", false);
-   xhr.send(null);
-   xhr.onreadystatechange = function() {
-     if ( xhr.readyState === 4 && xhr.status === 200 ) {
-       var jsonContact = JSON.parse(xhr.responseText);
-       contactPopulate(jsonContact);
-     }
-   };
+function XHR(callback){
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4 && (xhr.status === 200 || xhr.status === 0)) {
+        //response code of 0 accepted due to local file system requirements
+            callback(xhr.responseText);
+        }
+    };
+    xhr.open('GET', 'contact-list-users.json', true);
+    xhr.send();
+}
+
+function init() {
+ XHR(function(response) {
+  // Parse JSON string into object
+    var actual_JSON = JSON.parse(response);
+    contactPopulate(actual_JSON);
+ });
+}
+
+init();
+
+// function loadJSON(callback) {
+//
+//     var xobj = new XMLHttpRequest();
+//         xobj.overrideMimeType("application/json");
+//     xobj.open('GET', 'contact-list-users.json', true);
+//     xobj.onreadystatechange = function () {
+//           if (xobj.readyState == 4 && xobj.status == "200") {
+//             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+//             callback(xobj.responseText);
+//           }
+//     };
+//     xobj.send(null);
+//  }
+//
+
 
 // CONTACT LIST TOGGLE
 function slideToggle() {
-    var contactList = document.getElementById('contacts-list');
-    if(contactList.className == 'contacts-list contacts-hide') {
+    var contactList = document.getElementById('hide-container');
+    if(contactList.className == 'hide-container contacts-hide') {
       contactList.classList.toggle('contacts-hide');
     }
     else {
